7fbf907152b6bbf3f83cd97e5e7d5b4d117f91ba
Patwalishita <31007028+Patwalishita@users.noreply.github.com>
2017-10-01 21:51:27 +0530

    Update p14.c

--- p14.c
+++ p14.c
@@ -1,3 +1,153 @@
+		
+//#include<stdio.h>
+//#include<windows.h>
+//#include<stdlib>
+int fileopen(FILE *);
+void plac_stat()
+{
+	FILE *fp;
+	char ch;
+	int n,m,r;
+	outer :
+ 	g1:
+ 	printf("\n\n\n");
+	printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+	printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+	printf("\t\t\t\t\t\t\t1. Enter 1 to view the details of year -->  2015\n\n\n\n");
+	printf("\t\t\t\t\t\t\t2. Enter 2 to view the details of year -->  2016\n\n\n\n");
+	printf("\t\t\t\t\t\t\t3. Enter 3 to view the details of year -->  2017\n\n\n\n");
+	printf("\t\t\t\t\t\t\t4. Enter 0 to exit\n\n\n\n");
+	printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+	printf("\t\t\t\t\t\t$******************************************************************$\n\n\n");
+	printf("\t\t\t\t\t\t\tInput :");
+	scanf("%d",&m);
+	system("cls");	
+	switch(m)
+	{	
+		case 1:
+			
+			inner1 :
+		//	system("cls");	
+			printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+			printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+			printf("\t\t\t\t\t\t\t1. Enter 1 to view the details of -->  WIPRO\n\n\n\n");
+			printf("\t\t\t\t\t\t\t2. Enter 2 to view the details of -->  TCS\n\n\n\n");
+			printf("\t\t\t\t\t\t\t3. Enter 3 to view the details of -->  INFOYSIS\n\n\n\n");
+			printf("\t\t\t\t\t\t\t4. Enter 4 to go back\n\n\n\n");
+			printf("\t\t\t\t\t\t\t5. Enter 0 to exit\n\n\n\n");
+			printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+			printf("\t\t\t\t\t\t$******************************************************************$\n\n\n");
+			printf("\t\t\t\t\t\t\tInput :");
+			scanf("%d",&n);
+			system("cls");
+			switch(n)
+			{
+				case 1:
+					fp=fopen("w2015.txt","r");
+					r=fileopen(fp);
+					if(r==10)
+					goto g1;
+					fclose(fp);
+					break;
+				case 2:
+					fp=fopen("T2015.txt","r");
+					r=fileopen(fp);
+					if(r==10)
+					goto g1;
+					fclose(fp);
+					break;
+				case 3:
+					fp=fopen("I2015.txt","r");
+					r=fileopen(fp);
+					if(r==10)
+					goto g1;
+					fclose(fp);
+					break;
+				case 4:
+					goto g1;
+					break;
+				case 0:	
+					//system("cls");
+				    //plac_stat();
+					//break;		
+					printf("\n\n\n\n\n\n\n");
+					printf("\t\t\t\t\t\t$**************************THANK YOU*******************************$\n\n");
+					printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+					break;	
+				default:
+				
+				system("cls");
+				printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+				printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+				printf("\t\t\t\t\t\tWrong input\n\n\n\t\t\t\t\t\tPlease enter given input:\n\n\n\n");
+			
+				goto inner1;
+				
+			}
+			break;
+			
+		case 2:
+			inner2 :
+			
+			printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+			printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+			printf("\t\t\t\t\t\t\t1. Enter 1 to view the details of -->  WIPRO\n\n\n");
+			printf("\t\t\t\t\t\t\t2. Enter 2 to view the details of -->  TCS\n\n\n");
+			printf("\t\t\t\t\t\t\t3. Enter 3 to view the details of -->  INFOYSIS\n\n\n");
+			printf("\t\t\t\t\t\t\t4. Enter 4 to go back\n\n\n\n");
+			printf("\t\t\t\t\t\t\t5. Enter 0 to exit\n\n\n\n");
+			printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+			printf("\t\t\t\t\t\t$******************************************************************$\n\n\n");
+			printf("\t\t\t\t\t\t\tInput :");
+			scanf("%d",&n);
+			system("cls");
+			switch(n)
+			{
+				case 1:
+					fp=fopen("w2016.txt","r");
+					r=fileopen(fp);
+					if(r==10)
+					goto g1;
+					fclose(fp);
+					break;
+				case 2:
+					fp=fopen("T2016.txt","r");
+					r=fileopen(fp);
+					if(r==10)
+					goto g1;
+					fclose(fp);
+					break;
+				case 3:
+					fp=fopen("I2016.txt","r");
+					r=fileopen(fp);
+					if(r==10)
+					goto g1;
+					fclose(fp);
+					break;
+				case 4:
+					goto g1;
+					break;
+				case 0:
+					//	system("cls");
+				   // plac_stat();
+				//	break;	
+					printf("\n\n\n\n\n\n\n");
+					printf("\t\t\t\t\t\t$**************************THANK YOU*******************************$\n\n");
+					printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+					break;
+				default:
+				
+				system("cls");
+				printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+				printf("\t\t\t\t\t\t$******************************************************************$\n\n");
+				printf("\t\t\t\t\t\tWrong input\n\n\n\t\t\t\t\t\tPlease enter given input:\n\n");
+			
+				goto inner2;
+					
+			}
+			break;
+
+
 		case 3:
 			inner3 :
 			//system("cls");
